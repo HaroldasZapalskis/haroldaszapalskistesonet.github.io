dd51b100dc18cc7113a920ad296c6b8ea5f66f97
HaroldasZapalskis <HAROLDAS.zapalskis@gmail.com>
2020-09-23 07:01:52 +0300

    Deploy

---
+++ precache-manifest.abee6b992c26e4425982bb7edc0b4b0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceb06829dad50971c0b944fca38e6be8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fbf50e7ba6540e56685",
+    "url": "/static/css/main.dadc48f3.chunk.css"
+  },
+  {
+    "revision": "5d60917640353fc114ff",
+    "url": "/static/js/2.3c41ed46.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3c41ed46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fbf50e7ba6540e56685",
+    "url": "/static/js/main.19e7fadf.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "97b147d8e3dba2e7a9bf3b38648c9e80",
+    "url": "/static/media/sorting.97b147d8.svg"
+  }
+]);
